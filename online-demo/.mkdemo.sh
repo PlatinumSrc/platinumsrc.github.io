b4e2a2a7770f906730c95b1ba88d0e895d897ce6
PQCraft <0456523@gmail.com>
2024-06-30 03:05:13 -0400

    Use latest engine release for demo
    Use the code at the latest stable release instead of possibly in-progress code at the default branch

--- online-demo/.mkdemo.sh
+++ online-demo/.mkdemo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-git clone --depth 1 https://github.com/PlatinumSrc/PlatinumSrc
+B="$(curl -s https://api.github.com/repos/PlatinumSrc/PlatinumSrc/releases/latest | jq -r .tag_name)"
+[ -z "$B" -o "$B" == "null" ] && gc() { git clone "$@"; } || gc() { git clone -b "$B" "$@"; }
+gc --depth 1 https://github.com/PlatinumSrc/PlatinumSrc
+unset gc B
 cd PlatinumSrc
 
 mkdir games mods
